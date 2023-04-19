33c6a1ef3bfa8c42b849a575d8df2add6a754ace
Hiolia <69538503+Hiolia@users.noreply.github.com>
2023-04-19 19:08:48 +0200

    Update GestionDonnee.java
    Liste de noms/prenoms remplie

--- GestionDonnee.java
+++ GestionDonnee.java
@@ -4,18 +4,23 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class GestionDonnee {
-	
+
 	ArrayList<Personne> listePersonne=new ArrayList<>();
-	ArrayList<Personne> listeHomme=new ArrayList<>();
-	ArrayList<Personne> listeFemme=new ArrayList<>();
+	ArrayList<PersonneT> listeHomme=new ArrayList<>();
+	ArrayList<PersonneT> listeFemme=new ArrayList<>();
 	public class creePersonne {
-		String[] prenomH= {"henri"};
-		String[] prenomF= {"jeanne"};
-		String[] nom= {"Boisjoli"};
-		String[] choixSexe={"homme","femme"};
-		String[] choixAttributs= {"bouclé"};
-		String[] choixPréférences= {"chat"};
-		String[] choixRejets= {"chien"};
+		String[] prenomF= {"Charlotte", "Francis", "Karen", "Ethel", "Catherine", "Rhonda", "Rene", "Virginia", "Elizabeth", "Samantha", "Elisha", "Tiffany", "Amy", "Erika", "Shelley", "Patricia", "Sharon", "Sally", "Kimberly", "Anna", "Gisele", "Dinah", "Ashley", "Dee", "Kimberly", "Jennifer", "Christie", "Ana", "Lynda", "Blanche", "Gladys", "Lu", "Beverly", "Kimberly", "Kathleen", "Claudia", "Nanette", "Debra", "Grace", "Lisa", "Kenna", "Linda", "Pat", "Angelica", "Laura", "Joan", "Marie", "Joanne", "Ana", "Gloria", "Stefanie", "Ella", "Nicole", "Sylvia", "Charlotte", "Carol", "Melissa", "Anne", "Leslie", "Candace", "Joy", "Vernita", "Lynne", "Eleanor", "Julie", "Iris", "Christina", "Audrey", "Linda", "Patricia", "Michelle", "Nichole", "Julia", "Deborah", "Tiffany", "Marion", "Ester", "Rosemary", "April", "Rebecca", "Michelle", "Sarah", "Misty", "Kim", "Jeannie", "Helen", "Vera", "Velva", "Nancy", "Marianne", "Jean", "Evelyn", "Shana", "Dorothy", "Irma", "Sandra", "Susan", "Nora", "Mary", "Wanda"};
+		String[] prenomH= {"Donald", "Anthony", "James", "Jerry", "Michael", "Anton", "Steven", "Christian", "Tony", "James", "James", "Charles", "Gerard", "Philip", "Carlos", "Peter", "William", "Gary", "Derek", "Joseph", "Keith", "Philip", "Melvin", "Jean", "Robert", "Lewis", "Roy", "Daniel", "Adrian", "Allan", "Paul", "Dustin", "Edward", "Harley", "Joseph", "Lee", "Kenneth", "Kevin", "John", "Thomas", "Tyler", "James", "James", "Patrick", "Stephen", "John", "Gary", "Eric", "Curtis", "Albert", "Jayson", "John", "Kelly", "Jorge", "Vernon", "Evan", "Nathan", "Stan", "Michael", "Daniel", "Ronald", "Adrian", "Herman", "Paul", "Johnny", "Steven", "Rafael", "Edward", "Richard", "Jeffery", "Robert", "Daniel", "Thomas", "James", "James", "Michael", "Marshall", "Michael", "Peter", "Gary", "Steve", "James", "Carey", "Samuel", "George", "Robert", "Ashley", "Larry", "Robert", "Gary", "Wyatt", "John", "Ronald", "Steve", "Mark", "Jeffrey", "Ricky", "Anthony", "Daniel", "Sam"};
+		String[] nom= {"Taylor", "Ginn", "Myers", "Winslow", "Conroy", "Gebhart", "Anello", "Thompson", "Mirabal", "Wilson", "Blanding", "Mccray", "Hunter", "Koroma", "Wynne", "Wallace", "Grover", "Miller", "Skinner", "Alcorn", "Thomas", "Huffman", "Johnston", "Mathis", "Bernier", "Johnson", "Anspach", "Thao", "Scott", "Wright", "Delgado", "Alm", "Bryan", "Genet", "Ramirez", "Wilder", "Sutton", "Maloney", "Rahe", "Griffith", "Deane", "Warren", "Trantham", "Jones", "Wagner", "Gary", "Mcilhinney", "Dennis", "Washington", "Marino", "Guerrero", "Owens", "Rose", "Dorsey", "Scott", "Eye", "Wilson", "Lewis", "Lassiter", "Roberts", "Mathis", "Walker", "Cervera", "Perez", "Jackson", "Niesman", "Grullon", "Neely", "Stegall", "Fonseca", "Cheek", "Carpenter", "Bautista", "Smith", "Knight", "Vanschaick", "Michel", "Strahle", "Koverman", "Thomas", "Rae", "Boutelle", "Galindo", "Wasson", "Rojas", "Hill", "Carlson", "Peterson", "Baker", "Folk", "Thomas", "Williams", "Gilbert", "Velovic", "Ferrell", "Gonzalez", "Mash", "Atwood", "Shults", "Lewandowski"};
+		String[] choixSexe={"0","1"};
+		enum animal{
+			chat,
+			chien,
+			tortue,
+			lapin,
+			serpent
+		}
+		//String a=String.valueOf(animal.values()[new Random().nextInt(animal.values().length)]);  return a random value of an enum
 		Personne p;
 		public creePersonne() {
 			Random r=new Random();
@@ -30,17 +35,17 @@ public class GestionDonnee {
 			ArrayList<String> attributs=new ArrayList<String>();
 			ArrayList<String> préférences=new ArrayList<String>();
 			ArrayList<String> rejets=new ArrayList<String>();
-			p=new Personne(prenom+r.nextInt(50),nom[r.nextInt(nom.length)],r.nextInt(47)+18,sexe,80,choixSexe[r.nextInt(2)],(r.nextInt(7)+14)/10, attributs,préférences,rejets);
+			p=new Personne(prenom,nom[r.nextInt(nom.length)],r.nextInt(47)+18,choixSexe[r.nextInt(2)],choixSexe[r.nextInt(2)],String.valueOf(r.nextInt(2)),String.valueOf(r.nextInt(2)),String.valueOf(r.nextInt(2)),String.valueOf(r.nextInt(2)),String.valueOf(r.nextInt(2)),String.valueOf(r.nextInt(2)));
 		}
 	}
-	
+
 	public ArrayList<Personne> rechercheSexe(String sexe) {
 		if (sexe=="homme") {
 			return this.listeHomme;
 		}
 		return this.listeFemme;
 	}
-	
+
 	public GestionDonnee() {
 		for(int i=0;i<50;i++) {
 			Personne perso=new creePersonne().p;
@@ -51,7 +56,7 @@ public class GestionDonnee {
 			else {
 				this.listeFemme.add(perso);
 			}
-				
+
 		}
 		System.out.println(this.listePersonne);
 	}
